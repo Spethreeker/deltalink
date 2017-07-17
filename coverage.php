dea6153ba87df16745bd5ab7ae5d4b4441eccc9a
Spethreeker <sallegrezz@gmail.com>
2017-07-17 16:41:16 -0500

    more includes

--- coverage.php
+++ coverage.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>Contact - Delta Link, LLC.</title>
-   <?php include("head.inc");?>
+<?php include("head.inc");?>
 </head>
 <body>
 <?php include("navbar.inc")?>
@@ -21,48 +21,10 @@
             <iframe src="http://sites.towercoverage.com/Default.aspx?mcid=14809&Acct=115" class="signup-iframe" frameborder="0">
             </iframe>
         </center>
-        </div>
-        <div class="column">
-        <div class="contact-column">
-          <span class="strip"> <i class="fa fa-address-book-o fa-2x" aria-hidden="true" style="float: left;">&nbsp;</i>
-            <h3 class="contact-heading">Phone Number</h3></span>
-                <blockquote><u>(662)-545-3467</u></blockquote>
-        </div>
-        <div class="contact-column">
-        <span class="strip"><i class="fa fa-file-text-o fa-2x" aria-hidden="true" style="float: left;">&nbsp;</i>
-        <h3 class="contact-heading">Physical Address</h3> 
-        </span>
-                <blockquote> 
-                631 Gaines Hwy, 
-                Boyle, MS 38730
-                </blockquote>
-        </div>
-        <div class="contact-column">
-        <span class="strip"><i class="fa fa-file-text-o fa-2x" aria-hidden="true" style="float: left;">&nbsp;</i>
-        <h3 class="contact-heading">Mailing Address</h3> 
-        </span>
-                <blockquote> 
-                PO Box 441,
-                Cleveland, MS 38732
-                </blockquote>
-        </div>
-        <div class="contact-column">
-           <span class="strip"><i class="fa fa-fax fa-2x" style="float: left;">&nbsp;</i>
-           <h3 class="contact-heading">Fax</h3></span>
-            <blockquote><u>1(887)-271-3465</u>
-            </blockquote>
-        </div>
-            <div class="contact-column">
-                <span class="strip"><i class="fa fa-envelope-o fa-2x" style="float: left;">&nbsp;</i><h3 class="contact-heading">Email Address</h3></span>
-                <blockquote>
-                    <address>
-                        <a class="email" href="mailto:info@deltalinknet.com">info@deltalinknet.com
-                        </a>
-                    </address>
-                </blockquote>
-            </div>
-        </div>
     </div>
+        
+    <?php include("contact_aside.inc");?>
+        </div>
     </div>
 </main>   
 <footer class="footer" style="margin-top: 100px;">
