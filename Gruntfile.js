10faaecd35912f708f2eed2c3ef13b9fbb332157
Spethreeker <sallegrezz@gmail.com>
2017-07-20 14:53:36 -0500

    cssmin works

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                seperator: ';'
+            },
+            dist: {
+                src: ['css/*.css'],
+                dest: 'css/concat.css'
+            }
+        },
+        cssmin: {
+            options: {
+                 mergeIntoShorthands: false,
+                roundingPrecision: -1
+  },
+                target: {
+                    files: {
+                    'css/output.css': ['css/bulma.css', 'css/stylesheet.css']
+                    }
+                }
+}
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['cssmin']);
+};
